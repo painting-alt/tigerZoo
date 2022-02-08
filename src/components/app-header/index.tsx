24783f0455c87c20bc9ff668214ca60f5bf07ea3
huaqi <2931246464@qq.com>
2022-02-08 21:34:35 +0800

    feat: 新增 header 样式

--- src/components/app-header/index.tsx
+++ src/components/app-header/index.tsx
@@ -1,27 +1,55 @@
+// 第三方组件
 import React, { memo } from 'react'
-import { Input, Button, Avatar } from 'antd'
-import { BulbOutlined, UserOutlined } from '@ant-design/icons'
+import { Space, Input, Button, Avatar } from 'antd'
+import { BulbOutlined, UserOutlined, SearchOutlined } from '@ant-design/icons'
 
+//样式相关
+import { Header } from './styled'
 // assets
 // .image 使用方式未知
-// import logo from '@/assets/img/logo.image'
-
-const { Search } = Input
 
 export default memo(function HQAppHeader() {
     return (
-        <header>
-            <img className='logo' src='' alt='' />
-            <div className='operationalZone'>
-                <Search
-                    placeholder='input search text'
-                    allowClear
-                    style={{ width: 200 }}
-                />
-                <Button type='primary'>发帖</Button>
-                <BulbOutlined />
-                <Avatar size={64} icon={<UserOutlined />} />
+        <Header>
+            <div className='actionBar'>
+                <div>
+                    <img className='logo' src='' alt='' />
+                </div>
+                <Space className='operationalZone' size='large'>
+                    <Input
+                        className='point search'
+                        bordered={false}
+                        style={{
+                            width: 240,
+                            height: 32,
+                            backgroundColor: '#e5e6eb',
+                            borderRadius: 5,
+                        }}
+                        placeholder='输入帖子标题、正文关键字'
+                        suffix={<SearchOutlined />}
+                    />
+                    <Button
+                        className='point'
+                        type='primary'
+                        style={{
+                            width: 90,
+                            borderRadius: 4,
+                            backgroundColor: '#4080ff',
+                        }}
+                    >
+                        发帖
+                    </Button>
+                    <BulbOutlined
+                        className='point'
+                        style={{ fontSize: '20px' }}
+                    />
+                    <Avatar
+                        className='point avatar'
+                        size={32}
+                        icon={<UserOutlined />}
+                    />
+                </Space>
             </div>
-        </header>
+        </Header>
     )
 })
---
+++ src/components/app-header/styled.tsx
@@ -0,0 +1,28 @@
+import styled from 'styled-components'
+
+const Header = styled.header<{}>`
+    box-sizing: border-box;
+    width: 100%;
+    height: 64px;
+    background-color: #ffffff;
+    border-bottom: 1px solid #f1f1f1;
+    box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 5%);
+
+    .actionBar {
+        box-sizing: border-box;
+        margin: 0 auto;
+        padding: 14px;
+        max-width: 1200px;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+
+        .operationalZone {
+            .point {
+                cursor: pointer;
+            }
+        }
+    }
+`
+
+export { Header }
