d9a292d356341403101ffa2d64b6f589fdb13050
huaqi <2931246464@qq.com>
2022-02-14 20:54:06 +0800

    fix: 发布路由跳转 bug 修复

--- src/views/login/components/login-password/index.tsx
+++ src/views/login/components/login-password/index.tsx
@@ -40,7 +40,7 @@ const LoginPassword = memo(() => {
                     <Checkbox>Remember me</Checkbox>
                 </Form.Item>
 
-                <a className='login-form-forgot' href=''>
+                <a className='login-form-forgot' href='/#'>
                     Forgot password
                 </a>
             </Form.Item>
--- src/views/main/cpns/publish-modal/index.tsx
+++ src/views/main/cpns/publish-modal/index.tsx
@@ -1,6 +1,6 @@
-import React, { memo, useEffect, useState} from 'react'
+import React, { memo, useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import {useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
 // 样式相关
 import { Modal, Button, Space, Input } from 'antd'
@@ -8,32 +8,34 @@ import { Modal, Button, Space, Input } from 'antd'
 // action
 import { changeTitleAction, changeTagAction } from '@/store/actionCreators'
 
-//网络请求
-import {publishData } from '@/network/publish'
+// 网络请求
+import { publishData } from '@/network/publish'
 
-// 参数类型
+// Publish 组件参数类型
 interface publishProps {
     visible: boolean
     setVisible: Function
 }
 
 const Publish: React.FC<publishProps> = memo(props => {
-    const [confirmLoading, setConfirmLoading] = React.useState(false)
-
     const { visible, setVisible } = props
+
+    const [confirmLoading, setConfirmLoading] = useState<boolean>(false)
+    const [title, setTitle] = useState<string>('')
+    const [istag, setIsTag] = useState<boolean>(true)
+    const [isPublish, setIsPublish] = useState<boolean>(true)
 
     const dispatch = useDispatch()
 
     const navigation = useNavigate()
 
-    let [title, setTitle] = useState('')
     const articleTitle = useSelector((state: any) => state.title)
     const article = useSelector((state: any) => state.article)
     const articleTag = useSelector((state: any) => state.tag)
-    
+
     // const handleOk =  async () => {
     //     setConfirmLoading(true)
-    //     // console.log(title);       
+    //     // console.log(title);
     //     setTimeout(() => {
     //         setVisible(false)
     //         setConfirmLoading(false)
@@ -48,28 +50,30 @@ const Publish: React.FC<publishProps> = memo(props => {
     //         })
     //         navigation('/show')
     // }
-    let handleOk:any;
-    useEffect(() => { 
+
+    let handleOk: any
+    useEffect(() => {
         // eslint-disable-next-line @typescript-eslint/no-unused-vars
-         // eslint-disable-next-line react-hooks/exhaustive-deps
-         handleOk = async ()=>{
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+        handleOk = async () => {
             setConfirmLoading(true)
-            // console.log(title);       
-            setTimeout(() => {
-                setVisible(false)
-                setConfirmLoading(false)
-            }, 2000)
-            // 发送网络请求，将store中的title、article、tag发送到后台
-            console.log(articleTitle, article, articleTag);
-               //在没有卸载之前发送网络请求
-                await publishData({
-                    title: articleTitle,
-                    content: article,
-                    tag: articleTag
-                })
-                navigation('/show')
+
+            /*  
+        在没有卸载之前发送网络请求
+        发送网络请求，将store中的title、article、tag发送到后台 
+        */
+            await publishData({
+                title: articleTitle,
+                content: article,
+                tag: articleTag,
+            })
+
+            setVisible(false)
+            setConfirmLoading(false)
+
+            // 页面跳转
+            navigation('/show')
         }
-    // handleOk()
     })
 
     const handleCancel = () => {
@@ -77,38 +81,51 @@ const Publish: React.FC<publishProps> = memo(props => {
         setVisible(false)
     }
 
-    const changeTitle = (el:any) => { 
+    const changeTitle = (el: any) => {
         // console.log(el.target.value)
         setTitle(el.target.value)
     }
 
-    const changeTitleOk = () => { 
+    const changeTitleOk = () => {
         dispatch(changeTitleAction(title))
+        setIsTag(false)
     }
 
-    const changeTag = (value:any) => { 
+    const changeTag = (value: any) => {
         // console.log(value)
         dispatch(changeTagAction(value))
+        setIsPublish(false)
     }
+
     return (
         <Modal
             title='发布板块'
             visible={visible}
             okText='发布文章'
+            okButtonProps={{ disabled: isPublish }}
             cancelText='取消发布'
-            onOk={()=>handleOk()}
+            onOk={() => handleOk()}
             confirmLoading={confirmLoading}
             onCancel={handleCancel}
         >
-            文章标题：<Input size='large'
+            文章标题：
+            <Input
+                size='large'
                 placeholder='请输入标题'
                 onChange={changeTitle}
                 onBlur={changeTitleOk}
-                style={{ width: "80%", marginBottom: "20px" }} />
+                style={{ width: '80%', marginBottom: '20px' }}
+            />
             <Space>
-                <Button onClick={()=>changeTag("综合讨论")}>综合讨论</Button>
-                <Button onClick={()=>changeTag("技术问答")}>技术问答</Button>
-                <Button onClick={()=>changeTag("笔记分享")}>笔记分享</Button>
+                <Button disabled={istag} onClick={() => changeTag('综合讨论')}>
+                    综合讨论
+                </Button>
+                <Button disabled={istag} onClick={() => changeTag('技术问答')}>
+                    技术问答
+                </Button>
+                <Button disabled={istag} onClick={() => changeTag('笔记分享')}>
+                    笔记分享
+                </Button>
             </Space>
         </Modal>
     )
