332554266d3a9fe8ef859fdf8a9b6af0d090de79
huaqi <2931246464@qq.com>
2022-02-17 18:00:41 +0800

    style: 退出登录样式修改

--- src/components/app-aside/components/userinfo/index.tsx
+++ src/components/app-aside/components/userinfo/index.tsx
@@ -1,53 +1,14 @@
-import React, { memo, useEffect } from 'react'
+import React, { memo } from 'react'
 
 // 样式相关
 import CardStyled from './styled'
-import { Card, Avatar, message } from 'antd'
+import { Card, Avatar } from 'antd'
 import { EditOutlined, SettingOutlined } from '@ant-design/icons'
-
-// redux 相关
-import { useDispatch, useSelector } from 'react-redux'
-import { lightSignout, resetSignout } from '@/store/user/actionCreators'
-import IAppState from '@/store/type'
-import { IUserState } from '@/store/user/type'
-
-import { useNavigate } from 'react-router-dom'
 
 const { Meta } = Card
 
 const Signout = memo(() => {
-    // 获取 dipatch、navigation 方法
-    const dispatch = useDispatch()
-    const navigation = useNavigate()
-
-    // 获取退出登录相关状态
-    const user = useSelector<IAppState, IUserState>(state => state.user)
-
-    // 监听状态
-    useEffect(() => {
-        // 退出登录成功
-        if (user.signout.loaded && user.signout.success) {
-            // 跳转至首页
-            navigation('/')
-        }
-
-        // 退出登录失败 显示失败的提示信息
-        if (user.signout.loaded && !user.signout.success) {
-            console.log(11)
-            message.error(user.signout.message)
-        }
-
-        return () => {
-            // 离开也面前重置退出登录状态
-            dispatch(resetSignout())
-        }
-    }, [navigation, user.signout, dispatch])
-
-    return (
-        <span className='signout' onClick={() => dispatch(lightSignout())}>
-            退出登录
-        </span>
-    )
+    return <span className='signout'>退出登录</span>
 })
 
 const Userinfo = memo(() => {
--- src/components/app-header/components/user-avatar/index.tsx
+++ src/components/app-header/components/user-avatar/index.tsx
@@ -1,12 +1,51 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
-import { Avatar, Menu, Dropdown } from 'antd'
-import { HomeOutlined, EditOutlined, UserOutlined } from '@ant-design/icons'
+// 样式相关
+import { Avatar, Menu, Dropdown, message } from 'antd'
+import {
+    HomeOutlined,
+    EditOutlined,
+    UserOutlined,
+    LogoutOutlined,
+} from '@ant-design/icons'
+
 import { useNavigate } from 'react-router-dom'
 
+//  redux 相关
+// redux 相关
+import { useDispatch, useSelector } from 'react-redux'
+import { lightSignout, resetSignout } from '@/store/user/actionCreators'
+import IAppState from '@/store/type'
+import { IUserState } from '@/store/user/type'
+
 export default function UserAvatar() {
-    // 获取 navigation 方法
+    // 获取 dispatch、navigation 方法
+    const dispatch = useDispatch()
     const navigation = useNavigate()
+
+    // 退出登录边界判断
+    // 获取退出登录相关状态
+    const user = useSelector<IAppState, IUserState>(state => state.user)
+
+    // 监听状态
+    useEffect(() => {
+        // 退出登录成功
+        if (user.signout.loaded && user.signout.success) {
+            // 跳转至首页
+            navigation('/')
+        }
+
+        // 退出登录失败 显示失败的提示信息
+        if (user.signout.loaded && !user.signout.success) {
+            console.log(11)
+            message.error(user.signout.message)
+        }
+
+        return () => {
+            // 离开也面前重置退出登录状态
+            dispatch(resetSignout())
+        }
+    }, [navigation, user.signout, dispatch])
 
     const jump = (payload: any) => {
         const { key } = payload
@@ -16,6 +55,9 @@ export default function UserAvatar() {
             navigation('/publish')
         } else if (key === '2') {
             navigation('user')
+        } else if (key === '3') {
+            // 退出登录
+            dispatch(lightSignout())
         }
     }
     const menu = (
@@ -28,6 +70,13 @@ export default function UserAvatar() {
             </Menu.Item>
             <Menu.Item key='2' icon={<UserOutlined />}>
                 我的主页
+            </Menu.Item>
+            <Menu.Item
+                key='3'
+                icon={<LogoutOutlined />}
+                style={{ borderTop: '1px solid #cccccc', color: 'red' }}
+            >
+                退出登录
             </Menu.Item>
         </Menu>
     )
